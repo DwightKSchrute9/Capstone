eec3ede28b05eac951b89104e4b9faab6023ebfe
DwightKSchrute9 <silviolampignano@gmail.com>
2023-03-28 10:32:10 +0200

    New_Repo_Capstone_PrimoCommit

---
+++ sitoScommesseCalcistiche/src/main/java/com/example/catBet/SitoScommesseCalcisticheApplication.java
@@ -0,0 +1,13 @@
+package com.example.catBet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitoScommesseCalcisticheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitoScommesseCalcisticheApplication.class, args);
+	}
+
+}
---
+++ sitoScommesseCalcistiche/src/test/java/com/example/catBet/SitoScommesseCalcisticheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catBet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitoScommesseCalcisticheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
