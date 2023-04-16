96659730e6621315c191a48d54a554577a793805
DwightKSchrute9 <silviolampignano@gmail.com>
2023-04-16 19:46:17 +0200

    implementazioneCarrelloScommesse

--- Angular/sito-scommesse/src/app/cart.service.ts
+++ Angular/sito-scommesse/src/app/cart.service.ts
@@ -1,4 +1,25 @@
-::::::yyy...
+import { Injectable } from "@angular/core";
+import { Bet } from "./bet/bet.model";
+import { Match } from "./matches/matches.model";
+import { BetType } from "./bet/betType.enum";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  bets: Bet[] = [];
+
+  addBet(match: Match, betType: string, odd: DoubleRange) {
+    if (!this.bets.find(existingBet => match.id === existingBet.match.id)) {
+      this.bets.push({match: match, betType: this.getBetType(betType), odd: odd});
+    }
+  }
+
+  getBetType(betType: string): BetType {
+    switch (betType) {
+      case "HOME": return BetType.HOME;
+      case "AWAY": return BetType.AWAY;
+      case "DRAW": default: return BetType.DRAW;
     }
   }
 }
